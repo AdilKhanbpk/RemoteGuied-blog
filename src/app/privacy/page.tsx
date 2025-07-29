3b615f29b87c70c4a4fde5656a623dedbd05e917
adilkhanbpk <adilkhanbpk@gmail.com>
2025-07-29 06:27:50 -0700

    Redesigned the policy page

--- src/app/privacy/page.tsx
+++ src/app/privacy/page.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import Layout from '@/components/layout/Layout';
 import { Card, CardContent } from '@/components/ui/Card';
+
+
 import type { Metadata } from 'next';
 
 export const metadata: Metadata = {
